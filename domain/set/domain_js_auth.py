a2f68ea993fc44fb1916733b3202fc9034a0fe7d
sailgu <juyou3hui@gmail.com>
2018-07-09 17:07:27 +0800

    domain set js.py  look info function has complete

--- domain/set/domain_js_auth.py
+++ domain/set/domain_js_auth.py
@@ -7,8 +7,13 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import sys
 import time
+import re
+import argparse
 
-
+parser = argparse.ArgumentParser(description='set domains in weixin open platform')
+parser.add_argument('account_tab', help='open platform account file')
+parser.add_argument('--set', help='if set, domain list')
+args = parser.parse_args()
 
 def find_by_try(find_method, select_str):
     status = 1
@@ -23,14 +28,21 @@ def find_by_try(find_method, select_str):
 
 def read_tsv(table, head = True):
     fh = open(table)
-    res
     if head:
         head = next(fh)
     return [_.strip().split() for _ in fh]
 
-def parser_openid_detail_page():
+def exist(find_method, select_str):
+    try:
+        driver.find_element_by_css_selector(select_str)
+    except:
+        return False
+    return True
 
+#def parser_openid_detail_page():
 
+if args.set:
+    domains_to_set = [_.strip() for _ in open(args.set)]
 
 driver = webdriver.Firefox()
 wait = WebDriverWait(driver, 5)
@@ -40,12 +52,14 @@ driver.maximize_window()
 
 language_switch = driver.find_element_by_css_selector('div.menu_placeholder')
 ActionChains(driver).move_to_element(language_switch).perform()
+time.sleep(0.1)
 driver.find_element_by_css_selector('li.menu_list_ele:nth-child(2)').click()
 driver.find_element_by_css_selector('#loginBarBt').click()
 
 
-for account_line in read_tsv(sys.argv[1]):
-    time.sleep(0.5)
+for account_line in read_tsv(args.account_tab):
+    # time.sleep(0.3)
+    driver.find_element_by_css_selector('#loginBarBt').click()
     driver.find_element_by_name('account').send_keys(account_line[3])
     driver.find_element_by_name('passwd').send_keys(account_line[4])
     driver.find_element_by_class_name('btn.btn_primary.btn_login').click()
@@ -57,9 +71,10 @@ for account_line in read_tsv(sys.argv[1]):
         detail_button = row.find_element_by_class_name('jsUrlLink')
         detail_button.click()
         driver.switch_to_window(driver.window_handles[1])
+        time.sleep(0.5)
 
         # fectch basic info (appid, gh_id, message_key) to store in table
-        open_title = driver.find_element_by_css_selector('.app_name')
+        open_title = driver.find_element_by_css_selector('.app_name').text
         appid = driver.find_element_by_css_selector('.app_infos > p:nth-child(1)')
         appid = appid.text.split(':')[1]
 
@@ -72,9 +87,14 @@ for account_line in read_tsv(sys.argv[1]):
         message_key = message_key.text
 
         ### set js share domain
-        # jump to domain set page
+        # enter in modify page
         modify_button = driver.find_element_by_css_selector('.btn_default')
         modify_button.click()
+        #time.sleep(0.3)
+        if exist(driver.find_elements_by_css_selector, 'span.btn:nth-child(1) > button'):
+            driver.find_element_by_css_selector('span.btn:nth-child(1) > button').click()
+
+        # jump to domain set page
         driver.find_element_by_css_selector('#nextBt').click()
         driver.find_element_by_css_selector('#js_next2').click()
 
@@ -85,45 +105,27 @@ for account_line in read_tsv(sys.argv[1]):
         # download verify text file
         domain_frame.find_element_by_css_selector('a')
 
+        # get seted domain
+        domain_input = domain_frame.find_element_by_css_selector('.frm_input')
+        domain_seted = domain_input.get_attribute('value')
+
         # set domain
-        if set_domain:
-            domain_input = domain_frame.find_element_by_css_selector('.frm_input')
-            domain_input.send_keys()
+        if args.set and len(domains_to_set)>0 and remain_times>0:
+            domain_input.send_keys(domains_to_set.pop())
             remain_times = remain_times - 1
 
-        #finally submit and login out
-        driver.find_element_by_css_selector('#submitBt').click()
+            #while(driver.find_element_by_class_name())
+            driver.find_element_by_css_selector('#submitBt').click()
 
+        # close current windows and switch to first
+        driver.close()
+        driver.switch_to_window(driver.window_handles[0])
 
+        # print out detail info
+        print '\t'.join(account_line[3:5]+
+                [open_title, appid, ghid, message_key, domain_seted, remain_times])
 
-
-
-
-
-
-
-        wait.until(EC.element_to_be_clickable(
-            (By.CSS_SELECTOR, 'a.btn.btn_default.js_promp'))).click()
-
-        try:
-            wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.js_btn'))).click()
-        except:
-            pass
-        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a#nextBt.btn.btn'))).click()
-        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a#js_next2'))).click()
-        mp_input = wait.until(EC.presence_of_element_located((By.NAME, 'white_mp')))
-        mp_input.clear()
-        mp_input.send_keys('')
-        domain_input = driver.find_element_by_css_selector('input[name=sns_domain]')
-        domain_input.clear()
-        domain_input.send_keys('')
-        submit = driver.find_element_by_css_selector('a#submitBt.btn.btn_primary.jsSendBt')
-        while driver.find_element_by_css_selector('a#submitBt.btn.btn_primary.jsSendBt'):
-            submit.click
-            time.sleep(0.5)
-
-
-
-
+    # finally login out
+    driver.find_element_by_css_selector("a.account_meta:nth-child(3)").click()
 
 
